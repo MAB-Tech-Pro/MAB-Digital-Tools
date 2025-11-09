daf058ef806833c438c1824911df27138e987e94
Mansab Ali Babar <themabtech@gmail.com>
2025-11-08 19:49:24 -0800

    Fix: themeColor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,6 +65,10 @@ export const metadata: Metadata = {
   },
 };
 
+export const viewport = {
+  themeColor: "#0ea5e9",
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
